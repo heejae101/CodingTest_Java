715b5b2bb8610efe9a1b732ab3c89bc94f3214b3
Chae Hee Jae <81417568+heejae101@users.noreply.github.com>
2023-11-21 21:39:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.11 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -7,14 +7,18 @@ class Solution {
         // k 번 이상 신고된 유저는 게시판 이용이 정지 해당 유저를 신고한 유저에게 정지사실을 메일로 발송한다.
         // 유저가 신고한 모든 내용을 취합하여 마지막에 한꺼번에 게시판 이용 정지를 시키면서 정지 메일을 발송합니다.
         // id_list 는 사용자들 report 앞은 신고한 사람 뒤는 신고 당한 사람 K는 정지 기준 (신고 횟수) result는 사용자가 통보 메일을 받는 횟수
-
+        
+        // 사용자 총 인원 수
         int size = id_list.length;
-        // 사용자가 통보 메일을 받는 횟수
+        // 사용자 고유 id 이메일 발송횟수
         int[] answer = new int[size];
-        int[] reportNum = new int[size];
+        // 신고 먹은 횟수 
+        int[] reportNum = new int[size]; 
+        
+        // 누가 누굴 신고햇는지 x가 신고한 사람 y 신고당한사람 
         int[][] reportArr = new int[size][size];
 
-        // 사용자의 인덱스를 보관하는 map 이유: for문 처리를 덜하기 위해서
+        // 사용자의 인덱스를 보관하는 map 이유: for문 처리를 덜하기 위해서 O(n)
         HashMap<String,Integer> idMap = new HashMap<>();
         for(int i=0; i<id_list.length; i++) idMap.put(id_list[i],i);
 
